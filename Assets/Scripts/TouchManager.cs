c04bb951d32e4df9ce42bf11ccb8d972984cd4f3
Arindam Bose <arindam.b@gatech.edu>
2016-01-29 23:40:14 -0500

    TouchManager Done

---
+++ Assets/Scripts/TouchManager.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class TouchManager : MonoBehaviour {
+
+
+    public TouchPool touchPool;
+    public LayerMask backgroundLayer;
+
+    public bool MouseDebug;
+
+    void Update(){
+
+        foreach ( Touch touch in Input.touches){
+            
+            Vector3 touchPos = ScreenToWorldPosition( touch.position );
+            _manageObject(touch.fingerId, touchPos, touch.phase);
+        }
+
+        if ( MouseDebug){
+            Vector3 touchPos = ScreenToWorldPosition( Input.mousePosition);
+            if(Input.GetMouseButtonDown(0)) {               
+                _manageObject(0, touchPos, TouchPhase.Began);
+             //   Debug.Log("Mouse Clicked");
+            }
+            if(Input.GetMouseButton(0)){
+                _manageObject(0, touchPos, TouchPhase.Moved);
+               // Debug.Log("Mouse Moving " + Input.mousePosition);
+                Debug.DrawLine(Vector3.zero, touchPos);
+            }
+            if(Input.GetMouseButtonUp(0)) {
+                _manageObject(0, touchPos, TouchPhase.Ended);
+                //Debug.Log("Mouse Lifted");
+            }
+        }
+            
+
+    }
+
+    private void _manageObject(int touchIndex, Vector3 touchPos, TouchPhase phase){
+
+            
+        if( phase == TouchPhase.Began){
+                touchPool.ActivateObject(touchIndex, touchPos);
+            }
+
+        if( phase == TouchPhase.Moved || phase == TouchPhase.Stationary){
+                touchPool.UpdateObject(touchIndex, touchPos);
+            }
+
+        if( phase == TouchPhase.Ended || phase == TouchPhase.Canceled){
+                touchPool.DeactivateObject(touchIndex);
+            }
+        }
+
+
+
+
+    public Vector3 ScreenToWorldPosition(Vector2 screenPosition){
+
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+        RaycastHit hit;
+
+        if( Physics.Raycast(ray, out hit, 50.0f, backgroundLayer)){
+            Debug.Log("Touch world at "+ hit.point);
+        }
+
+        return new Vector3(hit.point.x, hit.point.y, 0);
+    }
+}
---
+++ Assets/Scripts/TouchPool.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TouchPool : MonoBehaviour {
+
+    public List<GameObject> touchObjects;
+
+
+    public GameObject getTouchObject(int i){
+        return touchObjects[i];
+    }
+
+    public void ActivateObject(int i, Vector3 position){
+
+        GameObject obj = getTouchObject(i);
+
+        obj.SetActive(true);
+
+        obj.transform.position = position;
+        Debug.Log("ACTIVATE OBJECT :"+ i);
+    }
+
+
+    public void DeactivateObject(int i){
+
+        GameObject obj = getTouchObject(i);
+
+        obj.SetActive(false);
+    }
+
+    public void UpdateObject(int i, Vector3 position){
+
+        GameObject obj = getTouchObject(i);
+
+        if ( ! obj.activeSelf ){
+            Debug.Log(i + "is inactive, activate it first");
+        }
+
+        obj.transform.position = position;
+
+    }
+}
