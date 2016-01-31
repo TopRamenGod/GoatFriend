0e27a88cf99e1ea84c95248775d5e72f9cf6500b
Arindam Bose <arindam.b@gatech.edu>
2016-01-31 10:21:33 -0500

    main menu done

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class MainMenuScript : MonoBehaviour {
+
+    public void StartGame(){
+        Scene scene = SceneManager.GetActiveScene();
+        int nextScene = scene.buildIndex;
+        nextScene++;
+
+        SceneManager.LoadScene(nextScene);
+    }
+}
